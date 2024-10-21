4812bb47eac65910c2d2c3659f652526fb768a61
HP319193 <davidharryporter@gmail.com>
2024-10-21 16:09:51 +0100

    inital commit

---
+++ popup.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const clickButton = document.getElementById('clickButton');
+
+    clickButton.addEventListener('click', async () => {
+        try {
+            chrome.runtime.sendMessage({ action: 'start' });
+        } catch (error) {
+            console.error('Error:', error);
+            outputDiv.textContent = 'An error occurred: ' + error.message;
+        }
+    });
+});
