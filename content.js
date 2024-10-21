01c4c0b9f7fa3faee207c5f79284d19c1f775679
HP319193 <davidharryporter@gmail.com>
2024-10-21 18:32:56 +0100

    length

--- content.js
+++ content.js
@@ -3,9 +3,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         // const more_but = document.querySelector("button#ember549");
         // if (more_but) sendResponse({ "status": "yes" })
         // else sendResponse({ "status": "no" })
-        setInterval(() => {
+        // setInterval(() => {
+
+        // }, 1000);
+
+        let counter = 0;
+        let intervalId = setInterval(() => {
             window.scrollBy(0, 200);
+            counter++;
+            console.log('Counter:', counter);
+
+            // Stop the interval after 5 iterations
+            if (counter >= 30) {
+                clearInterval(intervalId);
+                console.log('Interval stopped after 5 iterations');
+            }
         }, 1000);
+
+        persons = document.querySelectorAll("li.reusable-search__result-container");
+        sendResponse({ len: persons.length });
 
     }
 
